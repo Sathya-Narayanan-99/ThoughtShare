e456404001ddf58f63d1b6ec46bdee1f886b7729
Sathya <nanasathya@gmail.com>
2021-05-27 22:58:35 +0530

    Added cloudinary for storage.

--- ThoughtShare/settings.py
+++ ThoughtShare/settings.py
@@ -17,10 +17,14 @@ import os
 import dj_database_url
 import dotenv
 
+import cloudinary
+import cloudinary_storage
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STACIC_DIR = BASE_DIR/'static'
 DOTENV_FILE = BASE_DIR/'.env'
+CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -45,6 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'cloudinary',
+    'cloudinary_storage',
 
     'ckeditor',
 
@@ -137,7 +144,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [STACIC_DIR]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+# MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -155,3 +162,6 @@ CKEDITOR_CONFIGS = {
 
 options = DATABASES['default'].get('OPTIONS',{})
 options.pop('sslmode',None)
+
+CLOUDINARY_STORAGE = CLOUDINARY_URL
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
