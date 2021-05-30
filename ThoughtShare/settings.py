dc731365682835b8aac1ef08b7ca5d90fc7beefe
Sathya <nanasathya@gmail.com>
2021-05-30 23:49:57 +0530

    Removed media root

--- ThoughtShare/settings.py
+++ ThoughtShare/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [STACIC_DIR]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+# MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
